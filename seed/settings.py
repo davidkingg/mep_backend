cb7ef573771084f3ea5a065f5eeb4891cbbf06f7
davidkingg <davidoluseje@gmail.com>
2023-01-29 14:15:28 +0000

    static file setting

--- seed/settings.py
+++ seed/settings.py
@@ -155,7 +155,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'mediafiles')
 MEDIA_URL = '/media/'
 
